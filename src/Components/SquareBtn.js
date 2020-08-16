ada38896c34e1e29f7ac9066dede596bfd7676e5
k.nikolaev@opinionsn.com <QfPS5r4w3PqJ83H>
2020-08-16 22:06:06 +0300

    Updated SquareBtn.js

--- src/Components/SquareBtn.js
+++ src/Components/SquareBtn.js
@@ -1,16 +1,28 @@
+import React from "react";
+import PropTypes from 'prop-types';
 import Button from "@material-ui/core/Button";
 import styled from 'styled-components';
-import React from "react";
 
 const SquareBtn = styled(Button)`
-  ${({height, width}) => `
+  ${({height, width, stretch}) => `
   border-radius: 0;
   color: #fff;
   box-shadow: none;
+  padding: 0;
+  display: flex;
+  position: relative;
+  justify-content: ${stretch ? 'stretch' : 'center'};
+  align-items: ${stretch ? 'stretch' : 'center'};
   height: ${height || 50}px;
   width: ${width || 50}px;
   min-width: ${width || 50}px;
   `}
 `;
+
+SquareBtn.propTypes = {
+  height: PropTypes.number,
+  width: PropTypes.number,
+  stretch: PropTypes.bool
+};
 
 export default SquareBtn;
