1c1d561a01832632deb4c01ca4f534ceaa80ca8d
k.nikolaev@opinionsn.com <QfPS5r4w3PqJ83H>
2020-08-20 15:05:45 +0300

    Added EnhancedTooltip.js components

---
+++ src/Components/EnhancedTooltip.js
@@ -0,0 +1,40 @@
+import React, {memo} from 'react';
+import useTheme from "@material-ui/core/styles/useTheme";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Tooltip from "@material-ui/core/Tooltip";
+
+const EnhancedTooltip = ({children, ...restProps}) => {
+  const theme = useTheme();
+
+  const mdUp = useMediaQuery(theme.breakpoints.up('md'));
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
+
+  if (mdUp) {
+    return <Tooltip {...restProps}>{children}</Tooltip>
+  } else {
+    return <ClickAwayListener onClickAway={handleTooltipClose}>
+      <Tooltip
+        PopperProps={{
+          disablePortal: true,
+        }}
+        onClose={handleTooltipClose}
+        open={open}
+        {...restProps}
+      >
+        {React.cloneElement(children, {onClick: handleTooltipOpen})}
+      </Tooltip>
+    </ClickAwayListener>
+  }
+};
+
+export default memo(EnhancedTooltip);
