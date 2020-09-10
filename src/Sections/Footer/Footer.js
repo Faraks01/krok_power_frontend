d34208381004218d677ac7810265ca71536e9456
arez <k.nikolaev@opinionsn.com>
2020-09-10 17:08:32 +0300

    In desktop footer fixed logo position

--- src/Sections/Footer/Footer.js
+++ src/Sections/Footer/Footer.js
@@ -16,27 +16,27 @@ import {withRouter} from "react-router";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: 'black',
-    width: '100%',
-    padding: '38px 40px 33px'
+	backgroundColor: 'black',
+	width: '100%',
+	padding: '38px 40px 33px'
   },
 
   rootM: {
-    backgroundColor: 'black',
-    width: '100%',
-    padding: '20px 20px 10px'
+	backgroundColor: 'black',
+	width: '100%',
+	padding: '20px 20px 10px'
   },
 
   list: {
-    width: 'fit-content'
+	width: 'fit-content'
   },
 
   underlined: {
-    textDecorationLine: 'underline'
+	textDecorationLine: 'underline'
   },
 
   w300: {
-    fontWeight: '300'
+	fontWeight: '300'
   }
 }));
 
@@ -49,239 +49,242 @@ const Footer = ({location}) => {
   const mdUp = useMediaQuery(theme.breakpoints.up('md'));
 
   function navigateTo(routeName) {
-    return () => {
-      if (location.pathname !== routeName) {
-        history.push(routeName);
-        window.scrollTo(0, 0);
-      }
-    }
+	return () => {
+	  if (location.pathname !== routeName) {
+		history.push(routeName);
+		window.scrollTo(0, 0);
+	  }
+	}
   }
 
   if (mdUp) {
-    return (
-      <Grid
-        container
-        className={classes.root}
-        alignItems={"stretch"}
-        direction={"column"}>
-        <Typography
-          className={classes.w300}
-          align={"center"}
-          variant={"h6"}
-          style={{color: 'white'}}>
-          Постоянные НЕ участники выставки Rus-хаэнд
-        </Typography>
+	return (
+	  <Grid
+		container
+		className={classes.root}
+		alignItems={"stretch"}
+		direction={"column"}>
+		<Typography
+		  className={classes.w300}
+		  align={"center"}
+		  variant={"h6"}
+		  style={{color: 'white'}}>
+		  Постоянные НЕ участники выставки Rus-хаэнд
+		</Typography>
 
-        <Box height={'55px'}/>
+		<Box height={'55px'}/>
 
-        <Grid
-          container
-          direction={"row"}
-          justify={"space-between"}
-          alignItems={"center"}>
-          <Grid
-            className={classes.list}
-            container
-            direction={"column"}>
-            <Typography
-              onClick={navigateTo(PrimaryPowerSupplyPage.type.routeName)}
-              className={`${classes.w300} pointer`}
-              variant={"h6"}
-              style={{color: 'white'}}>
-              Первичное электропитание
-            </Typography>
+		<Grid
+		  container
+		  direction={"row"}
+		  justify={"space-between"}
+		  alignItems={"center"}>
+		  <Grid
+			style={{flex: 1}}
+			className={classes.list}
+			container
+			direction={"column"}>
+			<Typography
+			  onClick={navigateTo(PrimaryPowerSupplyPage.type.routeName)}
+			  className={`${classes.w300} pointer`}
+			  variant={"h6"}
+			  style={{color: 'white'}}>
+			  Первичное электропитание
+			</Typography>
 
-            <Typography
-              onClick={navigateTo(BearingStructuresPage.type.routeName)}
-              className={`${classes.w300} pointer`}
-              variant={"h6"}
-              style={{color: 'white'}}>
-              Несущие конструкции
-            </Typography>
+			<Typography
+			  onClick={navigateTo(BearingStructuresPage.type.routeName)}
+			  className={`${classes.w300} pointer`}
+			  variant={"h6"}
+			  style={{color: 'white'}}>
+			  Несущие конструкции
+			</Typography>
 
-            <Typography
-              onClick={navigateTo(HiEndPage.type.routeName)}
-              className={`${classes.w300} pointer`}
-              variant={"h6"}
-              style={{color: 'white'}}>
-              HI-END
-            </Typography>
+			<Typography
+			  onClick={navigateTo(HiEndPage.type.routeName)}
+			  className={`${classes.w300} pointer`}
+			  variant={"h6"}
+			  style={{color: 'white'}}>
+			  HI-END
+			</Typography>
 
-            <Typography
-              onClick={navigateTo(AboutPage.type.routeName)}
-              className={`${classes.w300} pointer`}
-              variant={"h6"}
-              style={{color: 'white'}}>
-              О НАС
-            </Typography>
+			<Typography
+			  onClick={navigateTo(AboutPage.type.routeName)}
+			  className={`${classes.w300} pointer`}
+			  variant={"h6"}
+			  style={{color: 'white'}}>
+			  О НАС
+			</Typography>
 
-            <Typography
-              onClick={navigateTo(DocumentationPage.type.routeName)}
-              className={`${classes.w300} pointer`}
-              variant={"h6"}
-              style={{color: 'white'}}>
-              Документация
-            </Typography>
-          </Grid>
+			<Typography
+			  onClick={navigateTo(DocumentationPage.type.routeName)}
+			  className={`${classes.w300} pointer`}
+			  variant={"h6"}
+			  style={{color: 'white'}}>
+			  Документация
+			</Typography>
+		  </Grid>
 
-          <LogoSvgIcon/>
+		  <LogoSvgIcon/>
 
-          <Grid
-            className={classes.list}
-            container
-            direction={"column"}>
-            <Typography className={classes.w300} variant={"h6"} style={{color: 'white'}}>
-              Телефон: +7 (985) 646-01-36
-            </Typography>
+		  <div style={{flex: 1, display: 'flex', justifyContent: 'flex-end'}}>
+			<Grid
+			  className={classes.list}
+			  container
+			  direction={"column"}>
+			  <Typography className={classes.w300} variant={"h6"} style={{color: 'white'}}>
+				Телефон: +7 (985) 646-01-36
+			  </Typography>
 
-            <Typography className={classes.w300} variant={"h6"} style={{color: 'white'}}>
-              E-mail: krokodailpower@gmail.com
-            </Typography>
+			  <Typography className={classes.w300} variant={"h6"} style={{color: 'white'}}>
+				E-mail: krokodailpower@gmail.com
+			  </Typography>
 
-            <Typography className={classes.w300} variant={"h6"}>
-              {' '}
-            </Typography>
+			  <Typography className={classes.w300} variant={"h6"}>
+				{' '}
+			  </Typography>
 
-            <Typography className={classes.w300} variant={"h6"} style={{color: 'white'}}>
-              Адрес бюро: г. Москва, ул. Вересаева, д. 15
-            </Typography>
+			  <Typography className={classes.w300} variant={"h6"} style={{color: 'white'}}>
+				Адрес бюро: г. Москва, ул. Вересаева, д. 15
+			  </Typography>
 
-            <Typography className={classes.w300} variant={"h6"} style={{color: 'white'}}>
-              Адрес шоурума: г. Москва, ул. Бажова, д. 30
-            </Typography>
-          </Grid>
-        </Grid>
+			  <Typography className={classes.w300} variant={"h6"} style={{color: 'white'}}>
+				Адрес шоурума: г. Москва, ул. Бажова, д. 30
+			  </Typography>
+			</Grid>
+		  </div>
+		</Grid>
 
-        <Box height={'49px'}/>
+		<Box height={'49px'}/>
 
-        <Typography className={classes.w300} align={"center"} variant={"h6"} style={{color: 'white'}}>
-          © 2020, Absolute Systems
-        </Typography>
+		<Typography className={classes.w300} align={"center"} variant={"h6"} style={{color: 'white'}}>
+		  © 2020, Absolute Systems
+		</Typography>
 
-      </Grid>
-    )
+	  </Grid>
+	)
   } else {
-    return (
-      <Grid
-        container
-        className={classes.rootM}
-        alignItems={"stretch"}
-        direction={"column"}>
-        <Grid
-          container
-          direction={"row"}
-          justify={"space-between"}
-          alignItems={"flex-start"}>
-          {mdUp && <LogoSvgIcon height={31.39} width={45}/>}
+	return (
+	  <Grid
+		container
+		className={classes.rootM}
+		alignItems={"stretch"}
+		direction={"column"}>
+		<Grid
+		  container
+		  direction={"row"}
+		  justify={"space-between"}
+		  alignItems={"flex-start"}>
+		  {mdUp && <LogoSvgIcon height={31.39} width={45}/>}
 
-          <Grid
-            item
-            xs={12}
-            className={classes.list}
-            container
-            alignItems={"flex-end"}
-            direction={"column"}>
-            {!mdUp && <Grid
-              item
-              container
-              xs={12}
-              justify={"space-between"}
-              direction={"row"}>
-              <LogoSvgIcon height={31.39} width={45}/>
+		  <Grid
+			item
+			xs={12}
+			className={classes.list}
+			container
+			alignItems={"flex-end"}
+			direction={"column"}>
+			{!mdUp && <Grid
+			  item
+			  container
+			  xs={12}
+			  justify={"space-between"}
+			  direction={"row"}>
+			  <LogoSvgIcon height={31.39} width={45}/>
 
-              <Grid direction={"column"}>
-                <Typography variant={"body1"} style={{color: 'white'}}>
-                  +7 (985) 646-01-36
-                </Typography>
+			  <Grid direction={"column"}>
+				<Typography variant={"body1"} style={{color: 'white'}}>
+				  +7 (985) 646-01-36
+				</Typography>
 
-                <Typography variant={"body1"} style={{color: 'white'}}>
-                  krokodailpower@gmail.com
-                </Typography>
-              </Grid>
-            </Grid>}
+				<Typography variant={"body1"} style={{color: 'white'}}>
+				  krokodailpower@gmail.com
+				</Typography>
+			  </Grid>
+			</Grid>}
 
-            {mdUp && <>
-              <Typography variant={"body1"} style={{color: 'white'}}>
-                +7 (985) 646-01-36
-              </Typography>
+			{mdUp && <>
+			  <Typography variant={"body1"} style={{color: 'white'}}>
+				+7 (985) 646-01-36
+			  </Typography>
 
-              <Typography variant={"body1"} style={{color: 'white'}}>
-                mf21@live.ru
-              </Typography>
-            </>}
+			  <Typography variant={"body1"} style={{color: 'white'}}>
+				mf21@live.ru
+			  </Typography>
+			</>}
 
-            <Box height={'16px'}/>
+			<Box height={'16px'}/>
 
-            <Typography variant={"body1"} style={{color: 'white'}}>
-              Адрес бюро: г. Москва, ул. Вересаева, д. 15
-            </Typography>
+			<Typography variant={"body1"} style={{color: 'white'}}>
+			  Адрес бюро: г. Москва, ул. Вересаева, д. 15
+			</Typography>
 
-            <Typography variant={"body1"} style={{color: 'white'}}>
-              Адрес шоурума: г. Москва, ул. Бажова, д. 30
-            </Typography>
-          </Grid>
-        </Grid>
+			<Typography variant={"body1"} style={{color: 'white'}}>
+			  Адрес шоурума: г. Москва, ул. Бажова, д. 30
+			</Typography>
+		  </Grid>
+		</Grid>
 
-        <Box height={'29px'}/>
+		<Box height={'29px'}/>
 
-        <Grid
-          className={classes.list}
-          container
-          direction={"column"}>
-          <Typography
-            onClick={navigateTo(PrimaryPowerSupplyPage.type.routeName)}
-            className={`${classes.underlined} pointer`}
-            variant={"body2"}
-            style={{color: 'white'}}>
-            Первичное электропитание
-          </Typography>
+		<Grid
+		  className={classes.list}
+		  container
+		  direction={"column"}>
+		  <Typography
+			onClick={navigateTo(PrimaryPowerSupplyPage.type.routeName)}
+			className={`${classes.underlined} pointer`}
+			variant={"body2"}
+			style={{color: 'white'}}>
+			Первичное электропитание
+		  </Typography>
 
-          <Typography
-            onClick={navigateTo(BearingStructuresPage.type.routeName)}
-            className={`${classes.underlined} pointer`}
-            variant={"body2"}
-            style={{color: 'white'}}>
-            Несущие конструкции
-          </Typography>
+		  <Typography
+			onClick={navigateTo(BearingStructuresPage.type.routeName)}
+			className={`${classes.underlined} pointer`}
+			variant={"body2"}
+			style={{color: 'white'}}>
+			Несущие конструкции
+		  </Typography>
 
-          <Typography
-            onClick={navigateTo(HiEndPage.type.routeName)}
-            className={`${classes.underlined} pointer`}
-            variant={"body2"}
-            style={{color: 'white'}}>
-            HI-END
-          </Typography>
+		  <Typography
+			onClick={navigateTo(HiEndPage.type.routeName)}
+			className={`${classes.underlined} pointer`}
+			variant={"body2"}
+			style={{color: 'white'}}>
+			HI-END
+		  </Typography>
 
-          <Typography
-            onClick={navigateTo(AboutPage.type.routeName)}
-            className={`${classes.underlined} pointer`}
-            variant={"body2"}
-            style={{color: 'white'}}>
-            О НАС
-          </Typography>
+		  <Typography
+			onClick={navigateTo(AboutPage.type.routeName)}
+			className={`${classes.underlined} pointer`}
+			variant={"body2"}
+			style={{color: 'white'}}>
+			О НАС
+		  </Typography>
 
-          <Typography
-            onClick={navigateTo(DocumentationPage.type.routeName)}
-            className={`${classes.underlined} pointer`}
-            variant={"body2"}
-            style={{color: 'white'}}>
-            Документация
-          </Typography>
-        </Grid>
+		  <Typography
+			onClick={navigateTo(DocumentationPage.type.routeName)}
+			className={`${classes.underlined} pointer`}
+			variant={"body2"}
+			style={{color: 'white'}}>
+			Документация
+		  </Typography>
+		</Grid>
 
-        <Box height={'29px'}/>
+		<Box height={'29px'}/>
 
-        <Typography variant={"body2"} style={{color: 'white'}}>
-          Постоянные НЕ участники выставки Rus-хаэнд
-        </Typography>
+		<Typography variant={"body2"} style={{color: 'white'}}>
+		  Постоянные НЕ участники выставки Rus-хаэнд
+		</Typography>
 
-        <Box height={'11px'}/>
+		<Box height={'11px'}/>
 
-        <Typography align={"center"} variant={"body2"} style={{color: 'white'}}>
-          © 2020, Absolute Systems
-        </Typography>
-      </Grid>
-    )
+		<Typography align={"center"} variant={"body2"} style={{color: 'white'}}>
+		  © 2020, Absolute Systems
+		</Typography>
+	  </Grid>
+	)
   }
 };
 
