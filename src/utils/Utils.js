3c4be97a3baf35e97022613b8ee62cc0abb8d285
arez <k.nikolaev@opinionsn.com>
2020-12-28 00:39:51 +0300

    Added url parser utility

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -2,6 +2,18 @@ class Utils {
   static normalizeApiAssetsUrl(url) {
     return `${window.location.origin}/api/${url}`
   }
+
+  static urlParser(str) {
+    const regex = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
+
+    const r = str.match(regex);
+
+    if (r !== null) {
+      return r[0];
+    }
+
+    return null;
+  }
 }
 
 export default Utils
